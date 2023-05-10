d8a5b61e25ec34580617c0f6e2b09d9ac04e2a0f
Egon Elbre <egonelbre@gmail.com>
2023-05-10 10:09:22 +0300

    abc2ly: add repeat tests

--- abc2ly/main_test.go
+++ abc2ly/main_test.go
@@ -5,38 +5,61 @@ import (
 	_ "embed"
 	"flag"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/egonelbre/lilypond/abc2ly/abc"
 	"github.com/google/go-cmp/cmp"
 )
 
-//go:embed testdata/features.abc
-var featuresABC string
-
-//go:embed testdata/features.ly
-var featuresLy string
-
 var update = flag.Bool("update", false, "update expected output")
 
 func TestConvert(t *testing.T) {
-	book, warnings := abc.Parse(featuresABC)
-	for _, warn := range warnings {
-		t.Error(warn)
+	matches, err := filepath.Glob("testdata/*.abc")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, abcpath := range matches {
+		t.Run(filepath.Base(abcpath), func(t *testing.T) {
+			lypath := strings.TrimSuffix(abcpath, ".abc") + ".ly"
+
+			abcdata, err := os.ReadFile(abcpath)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			book, warnings := abc.Parse(string(abcdata))
+			for _, warn := range warnings {
+				t.Error(warn)
+			}
+
+			var out bytes.Buffer
+
+			convert := &Convert{Output: &out}
+			for _, tune := range book.Tunes {
+				convert.Tune(tune)
+			}
+
+			converted := out.String()
+
+			lydata, err := os.ReadFile(lypath)
+			diff := ""
+			if err != nil {
+				t.Error(err)
+				diff = "<LILYPOND MISSING>"
+			} else {
+				diff = cmp.Diff(string(lydata), converted)
+			}
+
+			if diff != "" {
+				t.Error(diff)
+				if *update {
+					os.WriteFile("testdata/features.ly", out.Bytes(), 0644)
+				}
+				t.Log("LILYPOND OUTPUT:\n", out.String())
+			}
+		})
 	}
 
-	var out bytes.Buffer
-	convert := &Convert{Output: &out}
-	for _, tune := range book.Tunes {
-		convert.Tune(tune)
-	}
-
-	diff := cmp.Diff(featuresLy, out.String())
-	if diff != "" {
-		t.Error(diff)
-		if *update {
-			os.WriteFile("testdata/features.ly", out.Bytes(), 0644)
-		}
-		t.Log("LILYPOND OUTPUT:\n", out.String())
-	}
 }
