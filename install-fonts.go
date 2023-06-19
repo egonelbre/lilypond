fb2357ccaaa8ea1239467bb5ee0bbde6fea3886c
Egon Elbre <egonelbre@gmail.com>
2023-06-19 15:42:43 +0300

    install-fonts: fixes

--- install-fonts.go
+++ install-fonts.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"os"
-	"os/exec"
 	"path/filepath"
+	"runtime"
 )
 
 func main() {
 	homedir, err := os.UserHomeDir()
 	check(err)
 
-	lilyfonts := filepath.Join(homedir, "/Programs/lilypond/share/lilypond/2.24.0/fonts")
+	var lilyfonts string
+	switch runtime.GOOS {
+	case "darwin":
+		lilyfonts = filepath.Join(homedir, "Programs", "lilypond", "share", "lilypond", "2.24.0", "fonts")
+	case "windows":
+		lilyfonts = filepath.Join(homedir, ".local", "share", "fonts")
+	default:
+		panic("unhandled os " + runtime.GOOS)
+	}
 
 	copyFiles("./fonts/*/otf/*.otf", filepath.Join(lilyfonts, "otf"))
 	copyFiles("./fonts/*/svg/*.svg", filepath.Join(lilyfonts, "svg"))
@@ -20,12 +28,16 @@ func main() {
 }
 
 func copyFiles(glob, targetDir string) {
-	files, err := filepath.Glob("./fonts/*/otf/*.otf")
+	files, err := filepath.Glob(glob)
 	check(err)
 
+	_ = os.MkdirAll(targetDir, 0755)
+
 	for _, file := range files {
-		// TODO: make cross-platform
-		_, err := exec.Command("cp", file, filepath.Join(targetDir, filepath.Base(file))).CombinedOutput()
+		data, err := os.ReadFile(file)
+		check(err)
+
+		err = os.WriteFile(filepath.Join(targetDir, filepath.Base(file)), data, 0644)
 		check(err)
 	}
 }
